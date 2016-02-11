ab66bf72ff7162d96bc7bab2a69cf4b062d02d8d
Hang Su <suhangpro@gmail.com>
2016-02-11 14:16:28 -0500

    finetuning for vehicle classification done; ~20% error rate on bing7

---
+++ cnn_finetune_init.m
@@ -0,0 +1,66 @@
+function net = cnn_finetune_init(imdb, net)
+
+opts.weightInitMethod = 'xavierimproved' ;
+opts.scale = 1; 
+opts.sessionStartIdx = [1 6];
+
+if ~exist('net', 'var') || isempty(net), 
+  net = 'imagenet-matconvnet-vgg-m';
+end
+
+if  ischar(net), 
+  net_path = fullfile('data','models',[net '.mat']);
+  if ~exist(net_path,'file'), 
+    fprintf('Downloading model (%s) ...', net) ;
+    vl_xmkdir(fullfile('data','models')) ;
+    urlwrite(fullfile('http://www.vlfeat.org/matconvnet/models', ...
+      [net '.mat']), net_path) ;
+    fprintf(' done!\n');
+  end
+  net = load(net_path);
+end
+
+net.layers{end} = struct('name','loss','type','softmaxloss'); 
+
+trainable_layers = find(cellfun(@(l) isfield(l,'weights'),net.layers));
+
+if ~isfield(net.meta, 'pretrain'), 
+  net.meta.pretrain = {};
+end
+net.meta.pretrain = [net.meta.pretrain net.meta];
+net.meta.pretrain{end} = rmfield(net.meta.pretrain{end}, 'pretrain'); 
+net.meta.trainOpts.sessions{1} = struct('startEpoch',opts.sessionStartIdx(1), ...
+  'layers',trainable_layers(end));
+net.meta.trainOpts.sessions{2} = struct('startEpoch',opts.sessionStartIdx(2), ...
+  'layers',trainable_layers(1:end-1));
+net.meta.classes.name = imdb.meta.classes;
+net.meta.classes.description = imdb.meta.classes;
+
+[h,w,in,out] = size(net.layers{end-1}.weights{1});
+out = numel(net.meta.classes.name); 
+net.layers{end-1}.weights = {init_weight(opts, h, w, in, out, 'single'), zeros(out, 1, 'single')};
+
+end
+
+% -------------------------------------------------------------------------
+function weights = init_weight(opts, h, w, in, out, type)
+% -------------------------------------------------------------------------
+% See K. He, X. Zhang, S. Ren, and J. Sun. Delving deep into
+% rectifiers: Surpassing human-level performance on imagenet
+% classification. CoRR, (arXiv:1502.01852v1), 2015.
+
+switch lower(opts.weightInitMethod)
+  case 'gaussian'
+    sc = 0.01/opts.scale ;
+    weights = randn(h, w, in, out, type)*sc;
+  case 'xavier'
+    sc = sqrt(3/(h*w*in)) ;
+    weights = (rand(h, w, in, out, type)*2 - 1)*sc ;
+  case 'xavierimproved'
+    sc = sqrt(2/(h*w*out)) ;
+    weights = randn(h, w, in, out, type)*sc ;
+  otherwise
+    error('Unknown weight initialization method''%s''', opts.weightInitMethod) ;
+end
+
+end
---
+++ cnn_vehicle.m
@@ -0,0 +1,95 @@
+function [net, info] = cnn_vehicle(varargin)
+
+opts.expDir = fullfile('data','exp') ;
+opts.numFetchThreads = 12 ;
+opts.train = struct() ;
+opts = vl_argparse(opts, varargin) ;
+if ~isfield(opts.train, 'gpus'), opts.train.gpus = []; end;
+
+% -------------------------------------------------------------------------
+%                                                             Prepare model
+% -------------------------------------------------------------------------
+
+imdb = get_imdb('bing7'); 
+net = cnn_finetune_init(imdb); 
+
+% -------------------------------------------------------------------------
+%                                                                     Learn
+% -------------------------------------------------------------------------
+
+trainable_layers = find(cellfun(@(l) isfield(l,'weights'),net.layers)); 
+lr = cellfun(@(l) l.learningRate, net.layers(trainable_layers),'UniformOutput',false); 
+trainOpts = rmfield(net.meta.trainOpts,'sessions'); 
+
+% session 1: finetune the last layer only
+for i = 1:numel(lr), 
+  l = trainable_layers(i); 
+  if ismember(l,net.meta.trainOpts.sessions{1}.layers), 
+    net.layers{l}.learningRate = lr{i}; 
+  else
+    net.layers{l}.learningRate = lr{i}*0; 
+  end
+end
+trainOpts.numEpochs = net.meta.trainOpts.sessions{2}.startEpoch - 1;
+[net, info] = cnn_train(net, imdb, getBatchFn(opts, net.meta), ...
+                      'expDir', opts.expDir, ...
+                      trainOpts, ...
+                      opts.train) ;
+
+% session 2: finetune all layers jointly
+for i = 1:numel(lr), 
+  l = trainable_layers(i); 
+  if ismember(l,net.meta.trainOpts.sessions{2}.layers), 
+    net.layers{l}.learningRate = lr{i}; 
+  else
+    net.layers{l}.learningRate = lr{i}*0; 
+  end
+end
+trainOpts.numEpochs = net.meta.trainOpts.numEpochs - net.meta.trainOpts.sessions{2}.startEpoch + 1;
+[net, info] = cnn_train(net, imdb, getBatchFn(opts, net.meta), ...
+                      'expDir', opts.expDir, ...
+                      trainOpts, ...
+                      opts.train) ;
+
+% -------------------------------------------------------------------------
+%                                                                    Deploy
+% -------------------------------------------------------------------------
+
+net = cnn_deploy(net) ;
+modelPath = fullfile(opts.expDir, 'net-deployed.mat')
+save(modelPath, '-struct', 'net') ;
+
+% -------------------------------------------------------------------------
+function fn = getBatchFn(opts, meta)
+% -------------------------------------------------------------------------
+bopts.numThreads = opts.numFetchThreads ;
+bopts.imageSize = meta.normalization.imageSize ;
+bopts.border = meta.normalization.border ;
+bopts.averageImage = meta.normalization.averageImage ;
+bopts.rgbVariance = meta.augmentation.rgbVariance ;
+bopts.transformation = meta.augmentation.transformation ;
+
+fn = @(x,y) getSimpleNNBatch(bopts,x,y) ;
+
+% -------------------------------------------------------------------------
+function [im,labels] = getSimpleNNBatch(opts, imdb, batch)
+% -------------------------------------------------------------------------
+images = strcat([imdb.imageDir filesep], imdb.images.name(batch)) ;
+isVal = ~isempty(batch) && imdb.images.set(batch(1)) ~= 1 ;
+
+if ~isVal
+  % training
+  im = cnn_get_batch(images, opts, ...
+                              'prefetch', nargout == 0) ;
+else
+  % validation: disable data augmentation
+  im = cnn_get_batch(images, opts, ...
+                              'prefetch', nargout == 0, ...
+                              'transformation', 'none') ;
+end
+
+if nargout > 0
+  labels = imdb.images.class(batch) ;
+end
+
+
